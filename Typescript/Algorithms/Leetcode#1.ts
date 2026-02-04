dab59638dfd14a6974e0e46d33694995fa4b87f8
Oishika <oc618@nyu.edu>
2026-02-03 16:32:22 -0800

    Another day another problem

---
+++ Typescript/Algorithms/Leetcode#1.ts
@@ -0,0 +1,20 @@
+function twoSumBruteForce(nums: number[], target: number): number[] {
+    let indices: number[] = [];
+    for (let i = 0; i < nums.length; i ++) {
+        for (let j = i + 1; j < nums.length; j ++) {
+            if (nums[i] + nums[j] == target) {
+                indices.push(i);
+                indices.push(j);
+                return indices;
+            }
+        }
+    }
+    return indices;
+};
+
+console.log("Two Sum Brute Force")
+console.log(twoSumBruteForce([2,7,11,15], 9))
+console.log(twoSumBruteForce([3,2,4], 6))
+console.log(twoSumBruteForce([3,3], 6))
+console.log(twoSumBruteForce([3,4], 6))
+console.log(twoSumBruteForce([3,4], 99))
