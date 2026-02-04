cdc14ed48fb7f1328e7bc22569b839e61f73e5dd
Oishika <oc618@nyu.edu>
2026-02-03 16:32:32 -0800

    Another day another problem

--- Typescript/Algorithms/Leetcode#1.ts
+++ Typescript/Algorithms/Leetcode#1.ts
@@ -1,20 +1,42 @@
 function twoSumBruteForce(nums: number[], target: number): number[] {
-    let indices: number[] = [];
-    for (let i = 0; i < nums.length; i ++) {
-        for (let j = i + 1; j < nums.length; j ++) {
-            if (nums[i] + nums[j] == target) {
-                indices.push(i);
-                indices.push(j);
-                return indices;
-            }
-        }
+  let indices: number[] = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        indices.push(i);
+        indices.push(j);
+        return indices;
+      }
     }
-    return indices;
-};
+  }
+  return indices;
+}
 
-console.log("Two Sum Brute Force")
-console.log(twoSumBruteForce([2,7,11,15], 9))
-console.log(twoSumBruteForce([3,2,4], 6))
-console.log(twoSumBruteForce([3,3], 6))
-console.log(twoSumBruteForce([3,4], 6))
-console.log(twoSumBruteForce([3,4], 99))
+console.log("Two Sum Brute Force");
+console.log(twoSumBruteForce([2, 7, 11, 15], 9));
+console.log(twoSumBruteForce([3, 2, 4], 6));
+console.log(twoSumBruteForce([3, 3], 6));
+console.log(twoSumBruteForce([3, 4], 6));
+console.log(twoSumBruteForce([3, 4], 99));
+
+function twoSumBruteEfficient(nums: number[], target: number): number[] {
+  let numsIndices: Map<number, number> = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    numsIndices.set(nums[i], i);
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    const otherNumber: number | undefined = numsIndices.get(target - nums[i]);
+    if (otherNumber) {
+      return [i, otherNumber];
+    }
+  }
+  return [];
+}
+
+console.log("Two Sum Efficient");
+console.log(twoSumBruteForce([2, 7, 11, 15], 9));
+console.log(twoSumBruteForce([3, 2, 4], 6));
+console.log(twoSumBruteForce([3, 3], 6));
+console.log(twoSumBruteForce([3, 4], 6));
+console.log(twoSumBruteForce([3, 4], 99));
